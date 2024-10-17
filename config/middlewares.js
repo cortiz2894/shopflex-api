386a577bb1b746eabcf86d001e15fb1e2ccd63db
Christian <cortiz2894@gmail.com>
2024-10-16 22:01:02 -0300

    middlewares for allow local

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,18 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3000", "https://tudominio.com"],
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
